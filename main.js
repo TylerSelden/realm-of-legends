b160db4fd716ed840c3f81edc8672a20ec8dfd9a
TylerSelden <tyler.selden@salesianum.org>
2023-07-06 14:24:41 -0400

    Enhance plain-text capabilities of devclient.html

--- main.js
+++ main.js
@@ -11,8 +11,17 @@ var clients = [];
 
 server.on('request', function(request) {
   var connection = request.accept(null, request.origin);
-  connection.send("Welcome!");
-  
+  connection.send(`
+ ____  _____ ____  _     _        ____  _____   _     _____ _____ _____ _      ____  ____ 
+/  __\\/  __//  _ \\/ \\   / \\__/|  /  _ \\/    /  / \\   /  __//  __//  __// \\  /|/  _ \\/ ___\\
+|  \\/||  \\  | / \\|| |   | |\\/||  | / \\||  __\\  | |   |  \\  | |  _|  \\  | |\\ ||| | \\||    \\
+|    /|  /_ | |-||| |_/\\| |  ||  | \\_/|| |     | |_/\\|  /_ | |_//|  /_ | | \\||| |_/|\\___ |
+\\_/\\_\\\\____\\\\_/ \\|\\____/\\_/  \\|  \\____/\\_/     \\____/\\____\\\\____\\\\____\\\\_/  \\|\\____/\\____/
+
+testing
+
+testing`);
+
   clients.push(connection);
   connection.on('message', function(msg) {
     msg = msg.utf8Data;
