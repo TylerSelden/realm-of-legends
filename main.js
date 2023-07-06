a89a7c419b82d86665b76f7d55bf715b79923508
TylerSelden <tyler.selden@salesianum.org>
2023-07-06 13:27:55 -0400

    Create dev client

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("testing");
+const webSocketServer = require('websocket').server;
+var http = require('http');
+
+var port = 8080;
+
+var httpServ = http.createServer();
+httpServ.listen(port);
+var server = new webSocketServer({ httpServer: httpServ });
+
+var clients = [];
+
+server.on('request', function(request) {
+  var connection = request.accept(null, request.origin);
+  connection.send("Welcome!");
+  
+  clients.push(connection);
+  connection.on('message', function(msg) {
+    msg = msg.utf8Data;
+    connection.send(msg);
+  });
+});
